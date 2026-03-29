5b7513d798b6c2f0450df607d39e0e8107a353c9
abhi001-q <abhishek987ff@gmail.com>
2026-03-29 14:22:56 +0545

    bulding CodeSync where developers can code together

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.css'
+function App() {
+  return (
+    <div>
+      <main
+      className="h-screen w-full bg-gray-900 gap-4 p-2 "
+      >
+        <aside
+        className='h-full w-1/4 bg-amber-50 rounded-lg'
+        >
+        </aside>
+        <section
+        className='w-3/4 bg-neutral-800 rounded-lg'
+        >
+
+        </section>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
