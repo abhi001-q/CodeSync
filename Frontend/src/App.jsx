1ee6fd5065201aa14fff0e29b2bc853e6450d345
abhi001-q <abhishek987ff@gmail.com>
2026-04-04 17:06:47 +0545

    feat: integrate collaborative editing with Yjs and Socket.IO
    - Added y-monaco, y-socket.io, and yjs dependencies to package.json for real-time collaboration features.
    - Updated App.jsx to implement Yjs document and Socket.IO provider for collaborative editing.
    - Changed SocketProvider to SocketIOProvider and updated connection URL.
    - Fixed typo in Editor component's onMount prop.
    - Adjusted layout styles for better responsiveness.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import "./index.css";
 import { Editor } from "@monaco-editor/react";
 import { MonacoBinding } from "y-monaco";
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useState } from "react";
 import * as Y from "yjs";
-import { SocketProvider } from "y-socket.io";
+import { SocketIOProvider } from "y-socket.io";
 
 function App() {
   const editorRef = useRef(null);
-  const ydoc = useMemo(() => Y.Doc(), []);
+  const ydoc = useMemo(() => new Y.Doc(), []);
   const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
 
   const handleMount = (editor) => {
     editorRef.current = editor;
 
     const provider = new SocketProvider(
-      "http://localhost:3000",
+      "http://localhost:5173",
       "monaco",
-      ydoc,{autoConnect: true}
+      ydoc,
+      { autoConnect: true },
     );
     const monacoBinding = new MonacoBinding(
       yText,
@@ -26,10 +27,27 @@ function App() {
       provider.awareness,
     );
   };
+  if (!username) {
+    <main className="h-screen w-full bg-gray-4 items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <input type="text"
+        placeholder="Enter your username"
+        className="p-2 rounded-lg bg-gray-800 text-white"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+        className="p-2 rounded-lg bg"
+        >
+          join
+        </button>
+      </div>
+    </main>;
+  }
 
   return (
     <div>
-      <main className="h-screen w-full bg-gray-900 gap-4 p-4 ">
+      <main className="flex h-screen w-full bg-gray-900 gap-4 p-4 ">
         <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
         <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
           <Editor
@@ -37,8 +55,8 @@ function App() {
             defaultLanguage="javascript"
             defaultValue="// some comment"
             theme="vs-dark"
-            OnMount={handleMount}
-          /> 
+            onMount={handleMount}
+          />
         </section>
       </main>
     </div>
